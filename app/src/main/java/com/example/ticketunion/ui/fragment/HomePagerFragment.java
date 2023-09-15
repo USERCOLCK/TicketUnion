1c92ce9cd18be31735c65e8c5f815009470c5da1
DESKTOP-BH59CI9\User <1092746617@qq.com>
2023-09-15 22:40:47 +0800

    将分类数据给到对应页面

--- app/src/main/java/com/example/ticketunion/ui/fragment/HomePagerFragment.java
+++ app/src/main/java/com/example/ticketunion/ui/fragment/HomePagerFragment.java
@@ -1,11 +1,24 @@
 package com.example.ticketunion.ui.fragment;
 
+import android.os.Bundle;
 import android.view.View;
 
 import com.example.ticketunion.R;
 import com.example.ticketunion.base.BaseFragment;
+import com.example.ticketunion.model.domain.Categories;
+import com.example.ticketunion.utils.Constasts;
+import com.example.ticketunion.utils.LogUtils;
 
 public class HomePagerFragment extends BaseFragment {
+
+    public static HomePagerFragment newInstance(Categories.DataBean category){
+        HomePagerFragment homePagerFragment = new HomePagerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(Constasts.KEY_HOME_PAGER_TITLE,category.getTitle());
+        bundle.putInt(Constasts.KEY_HOME_PAGER_MATERIAL_ID, category.getId());
+        homePagerFragment.setArguments(bundle);
+        return homePagerFragment;
+    }
     @Override
     protected int getRootViewId() {
         return R.layout.fragment_home_pager;
@@ -14,5 +27,15 @@ public class HomePagerFragment extends BaseFragment {
     @Override
     protected void initView(View rootView) {
         setUpState(State.SUCCESS);
+    }
+
+    @Override
+    protected void loadData() {
+        Bundle arguments = getArguments();
+        String title = arguments.getString(Constasts.KEY_HOME_PAGER_TITLE);
+        int materialId = arguments.getInt(Constasts.KEY_HOME_PAGER_MATERIAL_ID);
+        //Todo:加载数据
+        LogUtils.d(this,"title ------>"+title);
+        LogUtils.d(this,"materialId ------>"+materialId);
     }
 }
